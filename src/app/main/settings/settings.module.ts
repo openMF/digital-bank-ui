f3b1f152c9fb22a43831d6ff643102ef05a02bb8
RadheTians <radheramantiwari87@gmail.com>
2020-12-13 21:24:45 +0530

    Made change password form to center-aligned and add tooltips to form-fields

--- src/app/main/settings/settings.module.ts
+++ src/app/main/settings/settings.module.ts
@@ -20,7 +20,7 @@ import { MatIconModule } from '@angular/material/icon';
     NbInputModule,
     ReactiveFormsModule,
     MatIconModule,
-    MatTooltipModule
+    MatTooltipModule,
   ],
 })
 export class SettingsModule {}
